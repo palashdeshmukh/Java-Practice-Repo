3b533047509ca3ee4b12b3ca0436a298486d874c
palashdeshmukh <palash.deshmukh02@gmail.com>
2022-11-19 23:19:55 +0530

    added Hello My name

--- Main.java
+++ Main.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+import java.io.*;
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Hellow My Name is Khan");
     }
 }
